4db33ed99c5165c0af9deee4c010274c57a70f13
Jalal Uddin <jalal.exe@gmail.com>
2019-01-12 10:39:00 +0000

    adding initial project files

---
+++ sms4bd-console/sms4bd-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sms4bd_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
